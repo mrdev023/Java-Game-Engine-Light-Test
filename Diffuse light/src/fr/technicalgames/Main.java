63023152fed2df79e88619e671a80a3090d1d18a
Florian Richer (MrDev023) <trexricher1997@gmail.com>
2016-03-06 14:21:22 +0100

    Update to OpenGL 3.3

--- Diffuse light/src/fr/technicalgames/Main.java
+++ Diffuse light/src/fr/technicalgames/Main.java
@@ -45,7 +45,7 @@ public class Main {
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		windowID = glfwCreateWindow(WIDTH,HEIGHT,TITLE,NULL,NULL);
 		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 		glfwSetWindowPos(windowID,(vidmode.width()-WIDTH)/2,(vidmode.height()-HEIGHT)/2);
